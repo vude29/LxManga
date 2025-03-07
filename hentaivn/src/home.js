28ec22d7869ab1f5f6b4906eb892d411ff30c7dd
Vude29 <vude29@gmail.com>
2025-03-07 22:44:48 +0700

    Delete hentaivn directory

--- hentaivn/src/home.js
+++
@@ -1,7 +0,0 @@
-load('config.js');
-function execute() {
-    return Response.success([
-        {title: "Trang chủ", input: BASE_URL, script: "gen.js"}
-        {title: "Thể loại", input: BASE_URL + "/tim-truyen", script: "gen.js"}
-    ]);
-}
