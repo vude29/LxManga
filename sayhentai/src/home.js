827e0ea6dae90ede471fcfc404edbaca13493012
Vude29 <vude29@gmail.com>
2025-03-08 00:50:30 +0700

    Update home.js

--- sayhentai/src/home.js
+++ sayhentai/src/home.js
@@ -3,8 +3,7 @@ function execute() {
     return Response.success([
         { title: "Cập Nhật", input: BASE_URL + "/", script: "gen.js" },
         { title: "Manhwa", input: BASE_URL + "/genre/manhwa", script: "gen.js" },
-        { title: "Manga", input: BASE_URL + "/genre/manga", script: "gen.js" },
-        { title: "Manhua", input: BASE_URL + "/genre/manhua", script: "gen.js" },
-        { title: "Thể loại", input: BASE_URL + "/genre/genre", script: "gen.js" },
+        { title: "Loạn luân", input: BASE_URL + "/genre/loan-luan", script: "gen.js" },
+        { title: "loli", input: BASE_URL + "/genre/loli", script: "gen.js" },
     ]);
 }
