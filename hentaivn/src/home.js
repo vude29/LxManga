5ac94bf1e6c0ace95b39b0a1806efbe71e3eee98
Vude29 <vude29@gmail.com>
2025-03-07 22:41:01 +0700

    Update home.js

--- hentaivn/src/home.js
+++ hentaivn/src/home.js
@@ -1,7 +1,7 @@
 load('config.js');
 function execute() {
     return Response.success([
-        {title: "Lịch Sử", input: BASE_URL + "/lich-su", script: "gen.js"}
+        {title: "Trang chủ", input: BASE_URL, script: "gen.js"}
         {title: "Thể loại", input: BASE_URL + "/tim-truyen", script: "gen.js"}
     ]);
 }
